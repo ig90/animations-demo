d12b8d41c130fe14b245f0e4e9e2f4d0d75313e4
Igor <igor.filipiuk@gmail.com>
2019-03-06 22:52:39 +0100

    added group() for running parallel animations

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { fade, slide, fadeInAnimation, bounceOutLeftAnimation } from '../animations';
-import { transition, trigger, query, style, animate, useAnimation, animateChild } from '@angular/animations';
+import { transition, trigger, query, style, animate, useAnimation, animateChild, group } from '@angular/animations';
 // import { trigger, transition, state, style, animate } from '@angular/animations';
 
 @Component({
@@ -12,11 +12,13 @@ import { transition, trigger, query, style, animate, useAnimation, animateChild 
    // slide,
     trigger('todosAnimation', [
       transition(':enter', [
-        query('h1', [
-          style({transform: 'translateY(-20px)'}),
-          animate(1000)
-        ]),
-        query('@todoAnimation', animateChild())
+        group([
+          query('h1', [
+            style({transform: 'translateY(-20px)'}),
+            animate(1000)
+          ]),
+          query('@todoAnimation', animateChild())
+        ])
       ])
     ]),
     trigger('todoAnimation', [
