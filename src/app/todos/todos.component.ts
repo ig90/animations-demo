a2fbb2c8c2d98a25fed3485ccc3cf90d16eb8181
Igor <igor.filipiuk@gmail.com>
2019-03-07 01:36:35 +0100

    added stagger() to todoAnimation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { fade, slide, fadeInAnimation, bounceOutLeftAnimation } from '../animations';
-import { transition, trigger, query, style, animate, useAnimation, animateChild, group } from '@angular/animations';
+import { transition, trigger, query, style, animate, useAnimation, animateChild, group, stagger } from '@angular/animations';
 // import { trigger, transition, state, style, animate } from '@angular/animations';
 
 @Component({
-  selector: 'todos',
+  selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
-    //fade,
+   // fade,
    // slide,
     trigger('todosAnimation', [
       transition(':enter', [
@@ -17,19 +17,21 @@ import { transition, trigger, query, style, animate, useAnimation, animateChild,
             style({transform: 'translateY(-20px)'}),
             animate(1000)
           ]),
-          query('@todoAnimation', animateChild())
+          query('@todoAnimation',
+            stagger('200ms', animateChild()) // 200ms delay between animating each todo item
+          )
         ])
       ])
     ]),
     trigger('todoAnimation', [
-      transition(':enter',[
+      transition(':enter', [
           useAnimation(fadeInAnimation, {
               params: {
                   duration: '500ms',
               }
           })
       ]),
-      transition(':leave',[
+      transition(':leave', [
           style({ backgroundColor: 'red'}),
           animate(500),
           useAnimation(bounceOutLeftAnimation)
@@ -41,7 +43,11 @@ export class TodosComponent {
   items: any[] = [
     'task 1',
     'task 2',
-    'task 3'];
+    'task 3',
+    'task 4',
+    'task 5',
+    'task 6'
+  ];
 
   addItem(input: HTMLInputElement) {
     this.items.splice(0, 0, input.value);
@@ -49,7 +55,7 @@ export class TodosComponent {
   }
 
   removeItem(item) {
-    let index = this.items.indexOf(item);
+    const index = this.items.indexOf(item);
     this.items.splice(index, 1);
   }
 
